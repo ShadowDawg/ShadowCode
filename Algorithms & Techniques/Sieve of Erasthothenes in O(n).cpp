96b01b0982a5982caf690a084ef46375eae9a9cc
ShadowDawg <62737754+ShadowDawg@users.noreply.github.com>
2021-10-23 11:19:06 +0530

    Create Sieve of Erasthothenes in O(n).cpp

---
+++ Algorithms & Techniques/Sieve of Erasthothenes in O(n).cpp
@@ -0,0 +1,18 @@
+const int N = 10000001;
+bool is_prime[N] = {true};
+int spf[N] = {0};   // smallest prime factor
+vector <int> primes;
+
+void sieve () {
+	is_prime[0] = is_prime[1] = false;
+	for (int i = 2;i < N;++i) {
+		if (is_prime[i]) {
+			spf[i] = i;
+			primes.pb(i);
+		}
+		for (int j = 0;j < (int)primes.size() && primes[j] <= spf[i] && i * primes[j] < N;++j) {
+			is_prime[ i * primes[j] ] = false;
+			spf[ i * primes[j] ] = primes[j];
+		}
+	}
+}
